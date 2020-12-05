c5c04513e879389c02fca600f9a4f95acfd38468
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2020-12-06 05:24:19 +0700

    update gateway
    Signed-off-by: Aditya Rahman Sidqi <adityars.sidqi@gmail.com>

--- gateway/src/main/java/com/aditya/bukatoko/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/aditya/bukatoko/gateway/GatewayApplication.java
@@ -3,8 +3,16 @@ package com.aditya.bukatoko.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableDiscoveryClient
 public class GatewayApplication {
 
@@ -12,4 +20,21 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/category/**").filters(f ->
+						f.hystrix(c -> c.setName("category").setFallbackUri("forward:/fallback"))).uri("lb://category-service"))
+				.route(p -> p.path("/product/**").filters(f ->
+						f.hystrix(c -> c.setName("product").setFallbackUri("forward:/fallback"))).uri("lb://product-service"))
+				.build();
+	}
+
+	@GetMapping("/fallback")
+	public ResponseEntity<Object> fallback() {
+		System.out.println("fallback enabled");
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("fallback", "true");
+		return ResponseEntity.ok().headers(headers).body(null);
+	}
 }
